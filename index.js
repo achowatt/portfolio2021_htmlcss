ba56405a3cc41484a5ba9324d566a7408dfea396
Anna <anna.chowattanakul@gmail.com>
2021-05-03 01:21:00 -0400

    changed according to feedback

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 gsap.registerPlugin(ScrollTrigger);
 
 //For changing home page background
-const landing = document.querySelector(".landing-page");
-const main = document.querySelector("main");
-const nav = document.querySelector("nav ul");
-const image = document.querySelector(".display-image");
-const option = {
-  threshold: 0.51,
-};
-function callBack(entries) {
-  console.log(entries[0]);
-  if (!entries[0].isIntersecting) {
-    main.classList.remove("blackBackground");
-    nav.classList.remove("whiteFont");
-    image.classList.add("border");
-  } else {
-    main.classList.add("blackBackground");
-    nav.classList.add("whiteFont");
-    image.classList.remove("border");
-  }
-}
-const observer = new IntersectionObserver(callBack, option);
-observer.observe(landing);
+// const landing = document.querySelector(".landing-page");
+// const main = document.querySelector("main");
+// const nav = document.querySelector("nav ul");
+// const image = document.querySelector(".display-image");
+// const option = {
+//   threshold: 0.51,
+// };
+// function callBack(entries) {
+//   console.log(entries[0]);
+//   if (!entries[0].isIntersecting) {
+//     main.classList.remove("blackBackground");
+//     nav.classList.remove("whiteFont");
+//     image.classList.add("border");
+//   } else {
+//     main.classList.add("blackBackground");
+//     nav.classList.add("whiteFont");
+//     image.classList.remove("border");
+//   }
+// }
+// const observer = new IntersectionObserver(callBack, option);
+// observer.observe(landing);
 
 //For changing images
 let timeline = gsap.timeline();
@@ -43,7 +43,7 @@ timeline
     { opacity: 0, scale: 2 },
     {
       duration: 1,
-      opacity: 0.7,
+      opacity: 1,
       scale: 1,
     }
   )
@@ -53,7 +53,7 @@ timeline
   })
   .to(".skills-img", {
     duration: 1,
-    opacity: 0.7,
+    opacity: 1,
   })
   .to(".skills-img", {
     duration: 1,
@@ -61,7 +61,7 @@ timeline
   })
   .to(".contact-img", {
     duration: 1,
-    opacity: 0.7,
+    opacity: 1,
   });
 
 ScrollTrigger.create({
